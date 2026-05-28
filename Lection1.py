4fdd8d6151b5b9a120f298061c5b894bcfcbc51e
Andreas Weidenbach <andreas.weidenbach@gmx.net>
2026-05-28 18:48:15 +0200

    First Commit

---
+++ Lection1.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+x = 5
+y = 10
+z = x * y
+print(x + y)
+print(z)
+
+age = (int(input("How old are you? ")))
+print("You are " + str(age) + " years old.")
+
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 100:
+    print("You are an adult.")
+else:
+    print("Invalid age.")
+    print("Please enter a valid age.")
+    print("Try again.")
+    
+
+    
+
+
